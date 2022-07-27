68f50b02902e8134860df4362242a463d987f8db
Dhruv-IITP <dhruv_2101cs26@iitp.ac.in>
2022-07-27 13:02:05 +0530

    insertion sort

--- sorts.c
+++ sorts.c
@@ -6,63 +6,77 @@ int main()
     printf("Enter the size of the array\n");
     scanf("%d", &n);
     int *ptr;
-    ptr=(int *)malloc(n*sizeof(int));
+    ptr = (int *)malloc(n * sizeof(int));
     printf("Enter array elements\n");
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d", ptr+i);
+        scanf("%d", ptr + i);
     }
     printf("The array to be sorted in ascending is \n {");
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d", *(ptr+i));
-        if(i!=n-1)printf(",");
+        printf("%d", *(ptr + i));
+        if (i != n - 1)
+            printf(",");
     }
     printf("}\n");
-    while(1)
+    while (1)
     {
-    int op;
-    printf("Select which sort you would like\n 1 Selection \t\t 2 Insertion \n 3 Bubble \t\t 4 Merge \n 5 Quick \nPlease make a choice\n");
-    scanf("%d", &op);
-    switch(op)
-    {
+        int op;
+        printf("Select which sort you would like\n 1 Selection \t\t 2 Insertion \n 3 Bubble \t\t 4 Merge \n 5 Quick \nPlease make a choice\n");
+        scanf("%d", &op);
+        switch (op)
+        {
         case 1:
         {
-        int posi, temp;
-        for(int c=0; c<n-1;c++)
-        {
-            posi=c;
-            for (int d = c + 1; d < n; d++)
+            int posi, temp;
+            for (int c = 0; c < n - 1; c++)
             {
-                if(*(ptr+posi)>*(ptr+d))
-                posi=d;
+                posi = c;
+                for (int d = c + 1; d < n; d++)
+                {
+                    if (*(ptr + posi) > *(ptr + d))
+                        posi = d;
+                }
+                if (posi != c)
+                {
+                    temp = *(ptr + c);
+                    *(ptr + c) = *(ptr + posi);
+                    *(ptr + posi) = temp;
+                }
             }
-            if(posi!=c)
-            {
-                temp=*(ptr+c);
-                *(ptr+c)=*(ptr+posi);
-                *(ptr+posi)=temp;
-            }
-        }
         }
         break;
 
         case 2:
         {
-            
+            int i, hold, j;
+            for (i = 1; i < n; i++)
+            {
+                hold = *(ptr+i);
+                j = i - 1;
+                while (j >= 0 && *(ptr+j) > hold)
+                {
+                    *(ptr+j+1)  = *(ptr+j);
+                    j = j - 1;
+                }
+                *(ptr+j+1)=hold;
+            }
         }
         break;
+
         default:
-        printf("\nPlease make a valid choice\n");
-        continue;
+            printf("\nPlease make a valid choice\n");
+            continue;
+        }
+        break;
     }
-    break;
-    }
-    printf("\nSorted array in asending order is\n{");
-    for(int i=0;i<n;i++)
+    printf("Sorted array in asending order is\n{");
+    for (int i = 0; i < n; i++)
     {
-        printf("%d", *(ptr+i));
-        if(i!=n-1)printf(",");
+        printf("%d", *(ptr + i));
+        if (i != n - 1)
+            printf(",");
     }
     printf("}\n");
 }
